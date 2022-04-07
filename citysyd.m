d1d85cf87fb81152e9e0271e596a10ae541b2225
siday <15045069763@163.com>
2022-04-07 12:47:58 +0800

    2022/4/7

---
+++ citysyd.m
@@ -0,0 +1,5 @@
+function m=citysyd(a,inn)
+    m=a^inn;
+end
+
+% m=citysyd( 5,6)
--- try33.m
+++ try33.m
@@ -1,5 +1,5 @@
-clear all;
-a=44*55;
+% clear all;
+a=citysyd(9,10);
 c=666/333;
 G_sys=tf([1,4],[1,4,20]);
 % bode(G_sys);
